c7f32106f30b79b3898d305b9cec1d47f906cf52
Junior Plantier <junior.plantier@gmail.com>
2021-08-15 22:11:36 -0300

    equals e hashcode

--- src/collections/plantier/mapas/Produto.java
+++ src/collections/plantier/mapas/Produto.java
@@ -10,18 +10,49 @@ public class Produto {
 		this.id = id;
 		this.descricao = descricao;
 	}
+	
 	public String getDescricao() {
 		return descricao;
 	}
+	
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
 	public int getId() {
 		return id;
 	}
+	
+	@Override
+	public int hashCode() {
+		//return (int) (Math.random() * 10); // Implementação errada; objetos iguais devem ir para o mesmo "balde"
+		
+		return String.valueOf(id).hashCode(); // ok!
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		
+		if (this == obj)
+			return true;
+		
+		if (!(obj instanceof Produto)){
+			return false;
+		}
+
+		Produto other = (Produto) obj;
+		if(this.id == other.getId()) {
+			return true;
+		}
+		return false;
+	}
+	
 	@Override
 	public String toString() {
 		return "Produto [id=" + id + ", descricao=" + descricao + "]";
 	}
-		
+	
 }
